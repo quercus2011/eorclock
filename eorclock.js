70344b75d8272378a49a8765d8dd51c763965a21
Quercus Maris <quercus2011@gmail.com>
2017-03-24 17:13:57 +0900

    Introduce Electron (with "Quick Start")

--- eorclock.js
+++ eorclock.js
@@ -1,14 +1,23 @@
 //! eorclock.js
-//! version : 0.1.0
 //! authors : Quercus Maris
 //! license : MIT
 //! https://github.com/quercus2011/eorclock
 
 ;(function (global, factory) {
-    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-    typeof define === 'function' && define.amd ? define(factory) :
-    global.eorclock = factory()
-}(this, function () { 'use strict';
-    var _eorclock = {};
-    return _eorclock;
+  if (typeof exports === 'object' && typeof module !== 'undefined') {
+    module.exports = factory();
+  } else if (typeof define === 'function' && define.amd) {
+    define(factory);
+  } else {
+    global.eorclock = factory();
+  }
+}(this, function () {
+  'use strict';
+
+  var _eorclock = function eorclockFactory() {
+    return function eorclock() {
+    };
+  };
+
+  return _eorclock;
 }));
